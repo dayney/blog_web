b08a478fca69a713ca2f99da022d5accf00f9cb2
krui <krui0728@gmail.com>
2019-06-11 22:38:42 +0800

    refactor::还有一点点问题

--- src/main.js
+++ src/main.js
@@ -3,12 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
+import store from './store/'
+import ElementUI from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.prototype.$http = Axios
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
