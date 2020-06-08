0d143a4f65e34e49858c92b7f12cc39bead200ef
krui <krui0728@gmail.com>
2020-06-08 18:31:55 +0800

    添加注释

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import { APP_HOST, APP_PORT } from "./config.ts";
-import router from "./routing.ts";
-import notFound from "./handlers/notFound.ts";
-import errorMiddleware from "./middlewares/error.ts";
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { APP_HOST, APP_PORT } from './config.ts';
+import router from './routing.ts';
+import notFound from './handlers/notFound.ts';
+import errorMiddleware from './middlewares/error.ts';
 
 const app = new Application();
 
--- services/db.ts
+++ services/db.ts
@@ -1,16 +1,16 @@
-import { DB_PATH } from "../config.ts";
-import { Todo } from "../models/todo.ts";
+import { DB_PATH } from '../config.ts';
+import { Todo } from '../models/todo.ts';
 
 export const fetchData = async (): Promise<Todo[]> => {
-  const data = await Deno.readFile(DB_PATH);
+	const data = await Deno.readFile(DB_PATH);
 
-  const decoder = new TextDecoder();
-  const decodedData = decoder.decode(data);
+	const decoder = new TextDecoder(); // 文本解码
+	const decodedData = decoder.decode(data);
 
-  return JSON.parse(decodedData);
+	return JSON.parse(decodedData);
 };
 
 export const persistData = async (data: Todo[]): Promise<void> => {
-  const encoder = new TextEncoder();
-  await Deno.writeFile(DB_PATH, encoder.encode(JSON.stringify(data)));
+	const encoder = new TextEncoder(); // 文本加密
+	await Deno.writeFile(DB_PATH, encoder.encode(JSON.stringify(data)));
 };
